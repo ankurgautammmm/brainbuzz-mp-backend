c1e8e238257d67b49317773d10d435890d92869a
ankurgautammmm <gautamankur0101@gmail.com>
2025-12-05 08:49:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ import jwt from "jsonwebtoken";
 import Groq from "groq-sdk";
 
 const groq = new Groq({
-  apiKey: process.env.GROQ_API_KEY, // FIXED !!!
+  apiKey: process.env.GROK_API_KEY, // FIXED !!!
 });
 
 // ----------------------------------------
@@ -259,3 +259,4 @@ Language: ${languageNote}.
 app.listen(PORT, () =>
   console.log(`✔ Server running at http://localhost:${PORT}`)
 );
+
